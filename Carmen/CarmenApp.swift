f9942e3ae42f737f4e09e179e8525b6ce08b5fa0
Sim <simeon@twolivesleft.com>
2023-10-12 14:17:52 +1030

    Initial Commit

---
+++ Carmen/CarmenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarmenApp.swift
+//  Carmen
+//
+//  Created by Sim Saens on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarmenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
